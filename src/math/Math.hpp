4af6f482aa59cfca52a198d76f5955f3a024c355
leod <subtle.frustration@proton.me>
2012-12-20 02:10:31 +0100

    Add more glm using statements.

--- src/math/Math.hpp
+++ src/math/Math.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 using glm::vec2;
 using glm::vec3;
 using glm::vec4;
 using glm::mat4;
+using glm::ivec2;
+using glm::ivec3;
+using glm::ivec4;
